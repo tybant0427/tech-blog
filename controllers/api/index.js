f9cce8cc67c94780144f55c11080600bb0e151fe
Tyler Bantley <tyler.bantley@icloud.com>
2022-02-09 10:54:13 -0600

    api index setup

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes.js');
+
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+
+
+module.exports = router; 
